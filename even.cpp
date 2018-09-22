ae8c044fe51cf433d690e775e4befb7a58953368
RArora28 <rishabharora1997@gmail.com>
2018-09-22 19:43:11 +0530

    added even with extension .cpp for indentation; fix this later

---
+++ even.cpp
@@ -0,0 +1,14 @@
+class Program {
+	int main() {
+		int n = read(); 
+		int arr[n]; 
+		for (i = 0, n) {
+			arr[i] = read(); 
+			if (arr[i] % 2 == 0) {
+				callout("printf", "%d ", arr[i]); 
+			}
+		}
+		callout("printf", "\n"); 
+		return 0; 
+	}
+}
