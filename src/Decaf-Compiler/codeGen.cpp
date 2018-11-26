cb34e83e686395081953668e5d8f2b79823597a2
RArora28 <rishabharora1997@gmail.com>
2018-11-26 13:29:40 +0530

    another test commit

--- src/Decaf-Compiler/codeGen.cpp
+++ src/Decaf-Compiler/codeGen.cpp
@@ -74,8 +74,83 @@ Value* methodDeclarations::codegen() {
 } 
 
 Value* methodDeclaration::codegen() {
-	
+	vector<pair < string, string > > args; 
+    vector<Type *> arguments;
+    auto arg_size = args.size();
+    for (auto param: listParams) {
+        /* Iterate over the arguments and get the types of them in llvm */
+        auto arg_type = param->type->type;
+        auto arg_name = param->name;
+        if (arg_type == "int") {
+            arguments.push_back(Type::getInt32Ty(Context));
+        } else if (arg_type == "boolean") {
+            arguments.push_back(Type::getInt1Ty(Context));
+        } else {
+            //errors++;
+            cout << "Arguments can only be int or boolean" << endl;
+            return nullptr;
+        }
+        args.push_back({arg_name, arg_type}); 
+    }
+
+    Type *retType;
+    switch (returnType->type) {
+    	case "int":		retType = Type::getInt32Ty(Context);
+		case "boolean":	retType = Type::getInt1Ty(Context);
+    	case "void":	retType = Type::getVoidTy(Context);
+    	default:    	{ // errors++;
+        	auto msg = "Invalid Return Type for " + methodName + ". Return Type can only be int or boolean";
+        	cout << msg << endl; 
+        	return nullptr;
+    	}
+    }
+    
+    /* Get the function type and create a Function */
+    FunctionType *FT = llvm::FunctionType::get(retType, arguments, false);
+    Function *F = llvm::Function::Create(FT, Function::ExternalLinkage, methodName, TheModule);
+
+    /* Iterate through arguments and set the Names for them */
+    int Idx = 0;
+    for (Function::arg_iterator AI = F->arg_begin(); Idx != arg_size; ++AI, ++Idx) {
+        AI->setName(argNames[Idx]);
+    }
+
+    /* Create a New block for this Function */
+    BasicBlock *BB = BasicBlock::Create(compilerConstructs->Context, "entry", F);
+    compilerConstructs->Builder->SetInsertPoint(BB);
+    Idx = 0;
+
+    /* Allocate memory for the arguments passed */
+    for (auto &Arg : F->args()) {
+        AllocaInst *Alloca = compilerConstructs->CreateEntryBlockAlloca(F, argNames[Idx], argTypes[Idx]);
+        compilerConstructs->Builder->CreateStore(&Arg, Alloca);
+        compilerConstructs->NamedValues[argNames[Idx]] = Alloca;
+        Idx++;
+    }
+
+    Value *RetVal = body->generateCode(compilerConstructs);
+    if (RetVal) {
+        /* make this the return value */
+        if (return_type != "void")
+            compilerConstructs->Builder->CreateRet(RetVal);
+        else
+            compilerConstructs->Builder->CreateRetVoid();
+        /// Iterate through each basic block in this function and remove any dead code
+        for (auto &basicBlock : *F) {
+            BasicBlock *block = &basicBlock;
+            removeDeadCode(block);
+        }
+        /* Verify the function */
+        verifyFunction(*F);
+        compilerConstructs->TheFPM->run(*F);
+        return F;
+    }
+
+    /* In case of errors remove the function */
+    F->eraseFromParent();
+	return nullptr;
 } 
+
 Value* parameterDeclarations::codegen() {} 
 Value* parameterDeclaration::codegen() {} 
 
